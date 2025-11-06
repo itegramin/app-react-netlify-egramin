3e284585c3c7aaf7c99f0993a9f95473c8ca5814
IT Egramin <itegramin@gmail.com>
2025-11-06 16:20:33 +0530

    uat host

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,19 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), netlify()],
+  server: {
+    allowedHosts: ['devserver-uat--egraminservices.netlify.app', 'egraminservices.in'],
+  },
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+      format: {
+        comments: false,
+      },
+    },
+  },
 })
