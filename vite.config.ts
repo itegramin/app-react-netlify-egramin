ac96f79efee236263604745c7a69a30c67ebc5be
IT Egramin <itegramin@gmail.com>
2025-11-06 13:05:14 +0530

    netlify initialze

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
+import netlify from "@netlify/vite-plugin";
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), netlify()],
 })
